0f6f8b579145fdefdd289bac06ae68c9e4217d8f
hungchihsueh <chihsuehhung@gmail.com>
2021-11-17 20:06:17 +0800

    update

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -6,19 +6,62 @@ import Banner from "../components/Course/CoursesBanner";
 import CoursesPageButton from "../components/Course/CoursesPageButton";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function ShoppingMain() {
+  const [hotProduct, setHotProduct] = useState([]);
+  const [product, setProduct] = useState([]);
+  useEffect(async () => {
+    //抓熱門商品
+    let resHitProduct = await axios.get(
+      "http://localhost:3001/product/hot-product",
+      { withCredentials: true }
+    );
+    setHotProduct(resHitProduct.data);
+
+    //抓全部商品
+    let resProduct = await axios.get("http://localhost:3001/product", {
+      withCredentials: true,
+    });
+    setProduct(resProduct.data);
+    console.log(resProduct.data);
+  }, []);
+
+  const hotList = hotProduct.map((product) => {
+    return (
+      <ProductCard
+        key={product.id}
+        name={product.title}
+        sold={product.sold}
+        part={product.body_part_id}
+        price={product.price}
+        rate={product.average_rate}
+      />
+    );
+  });
+  const pList = product.map((product) => {
+    return (
+      <ProductCard
+        key={product.id}
+        name={product.title}
+        sold={product.sold}
+        part={product.body_part_id}
+        price={product.price}
+        rate={product.average_rate}
+        styling={`marginEnd:"3rem"`}
+      />
+    );
+  });
   return (
     <>
       <Navbar />
       <Banner />
       {/* 熱門商品區 */}
-      <div className="product__hot container  justify-content-center align-items-center  ">
-        <h3 className="mb-4">熱門商品</h3>
-        <div className="product__list__card-group d-flex justify-content-around">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+      <div className="product__hot container  justify-content-center align-items-center mt-5 ">
+        <h3 className="mb-5">熱門商品</h3>
+        <div className="product__list__card-group d-flex justify-content-start">
+          {hotList}
         </div>
       </div>
       {/* 商品搜尋 */}
@@ -27,28 +70,10 @@ function ShoppingMain() {
       </div>
 
       {/* 商品列表 */}
-      <div className="d-flex flex-wrap container justify-content-between ">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+      <div className="ms-3">
+      <div className="d-flex flex-wrap container justify-content-start">
+        {pList}
+      </div>
       </div>
       <CoursesPageButton />
       <Footer />
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,19 +1,19 @@
 import * as React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Popover from "@mui/material/Popover";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 
-function ProductCard({ key, name, sold, rate, part, price }) {
+function ProductCard({ key, name, sold, rate, part, price, styling }) {
   //對部位
-  let bodyPart={
-    0:"綜合",
-    1:"手部",
-    2:"肩部",
-    3:"胸部",
-    4:"背部",
-    5:"腿部",
-};
+  let bodyPart = {
+    0: "綜合",
+    1: "手部",
+    2: "肩部",
+    3: "胸部",
+    4: "背部",
+    5: "腿部",
+  };
   //記數量
   const [number, setNumber] = useState(1);
   const handleCount = (i) => {
@@ -42,14 +42,14 @@ function ProductCard({ key, name, sold, rate, part, price }) {
   const id = open ? "simple-popover" : undefined;
   return (
     <>
-      <div class="card mx-4 mb-5" key={key}>
+      <div className={`card mx-5 mb-5`}  key={key}>
         <div className="card__addCart p-0">
           <Button
             aria-describedby={id}
             variant="contained"
             onClick={handleClick}
           >
-            <i class="fas fa-shopping-cart" />
+            <i className="fas fa-shopping-cart" />
           </Button>
           <Popover
             id={id}
@@ -87,26 +87,27 @@ function ProductCard({ key, name, sold, rate, part, price }) {
                 </button>
               </div>
               <button className="btn poper__cart__btn">
-                <i class="fas fa-shopping-cart"></i>
+                <i className="fas fa-shopping-cart"></i>
               </button>
             </div>
           </Popover>
         </div>
         <Link to="/product-single" className="text-decoration-none">
-          {/* <div className="card__crown"><i class="fas fa-crown"></i></div>
+          {/* <div className="card__crown"><i className="fas fa-crown"></i></div>
                     <p className="card__ranking">1</p> */}
 
           <img
             src="https://via.placeholder.com/200x150/333/FFECD2/?text=picture"
-            class="card-img-top"
+            className="card-img-top"
             alt="..."
           />
-          <div class="card-body">
+          <div className="card-body">
             <div className="card__price">${price}</div>
             <h5 className="card-title">
               {name}
               <span className="card-title__star">
-                {rate}<i class="fas fa-star"></i>
+                {rate}
+                <i className="fas fa-star"></i>
               </span>
             </h5>
             <div className="card__part">{bodyPart[part]}</div>
