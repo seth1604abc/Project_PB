6ee960ec88063d501c5dc38305881643248b1671
Eddie <90377819+Eddc125@users.noreply.github.com>
2021-11-19 23:51:30 +0800

    2021/11/19 - 把 map 從 components 中移出來

--- src/components/Course/CourseListContainer.js
+++ src/components/Course/CourseListContainer.js
@@ -1,7 +1,142 @@
 import React from 'react';
 import CourseControlBar from './CoursesControlBar';
-import CoursesSingleCourse from './CoursesCourseCard';
+import CoursesCourseCard from './CoursesCourseCard';
 import CoursesCoursePages from './CoursesPageButton';
+
+let item = [
+  {
+    id: "1",
+    title: "伏地挺身訓練-初級",
+    coach: "Ariel",
+    bodyparts: "手部",
+    count: "304",
+    created_at: "2015/10/20",
+    heart: 333,
+    level: "初級",
+    img: "./images/01.png",
+  },
+  {
+    id: "2",
+    title: "伏地挺身訓練-中級",
+    coach: "Taylor",
+    bodyparts: "胸部",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "中級",
+    img: "./images/02.png",
+  },
+  {
+    id: "3",
+    title: "伏地挺身訓練-高級",
+    coach: "Taylor",
+    bodyparts: "肩部",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "高級",
+    img: "./images/01.png",
+  },
+  {
+    id: "4",
+    title: "課程名稱稱",
+    coach: "Taylor",
+    bodyparts: "背部",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "初級",
+    img: "./images/02.png",
+  },
+  {
+    id: "5",
+    title: "課程名稱",
+    coach: "Taylor",
+    bodyparts: "部位",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "中級",
+    img: "./images/03.jpg",
+  },
+  {
+    id: "6",
+    title: "課程名稱",
+    coach: "Taylor",
+    bodyparts: "部位",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "高級",
+    img: "./images/03.jpg",
+  },
+  {
+    id: "7",
+    title: "課程名稱",
+    coach: "Taylor",
+    bodyparts: "部位",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "低級",
+    img: "./images/03.jpg",
+  },
+  {
+    id: "8",
+    title: "課程名稱",
+    coach: "Taylor",
+    bodyparts: "部位",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "下流",
+    img: "./images/01.png",
+  },
+  {
+    id: "9",
+    title: "課程名稱",
+    coach: "Taylor",
+    bodyparts: "部位",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "高級",
+    img: "./images/02.png",
+  },
+  {
+    id: "10",
+    title: "課程名稱",
+    coach: "Taylor",
+    bodyparts: "部位",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "初級",
+    img: "./images/03.jpg",
+  },
+  {
+    id: "11",
+    title: "課程名稱",
+    coach: "Taylor",
+    bodyparts: "部位",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "中級",
+    img: "./images/01.png",
+  },
+  {
+    id: "12",
+    title: "課程名稱",
+    coach: "Taylor",
+    bodyparts: "部位",
+    count: "505",
+    created_at: "2021/10/20",
+    heart: 123,
+    level: "高級",
+    img: "./images/01.png",
+  },
+];
 
 function CourseListContainer() {
   return (
@@ -9,7 +144,11 @@ function CourseListContainer() {
       <CourseControlBar />
       <div className="d-flex flex-wrap container justify-content-between mb-3 mt-4">
         {/* Card */}
-        <CoursesSingleCourse />
+        {item.map((item) => {
+        return (
+          <CoursesCourseCard key={item.id} id={item.id} title={item.title} coach={item.coach} bodyparts={item.bodyparts} count={item.count} created_at={item.created_at} heart={item.heart} level={item.level} img={item.img} />
+        );
+      })}
       </div>
       <div>
         {/* 頁碼 */}
--- src/components/Course/CoursesCourseCard.js
+++ src/components/Course/CoursesCourseCard.js
@@ -1,143 +1,7 @@
 import React from "react";
-import { useRef, useEffect, useState } from "react";
-
-let item = [
-  {
-    id: "1",
-    title: "伏地挺身訓練-初級",
-    coach: "Ariel",
-    bodyparts: "手部",
-    count: "304",
-    created_at: "2015/10/20",
-    heart: 333,
-    level: "初級",
-    img: "./images/01.png",
-  },
-  {
-    id: "2",
-    title: "伏地挺身訓練-中級",
-    coach: "Taylor",
-    bodyparts: "胸部",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "中級",
-    img: "./images/02.png",
-  },
-  {
-    id: "3",
-    title: "伏地挺身訓練-高級",
-    coach: "Taylor",
-    bodyparts: "肩部",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "高級",
-    img: "./images/01.png",
-  },
-  {
-    id: "4",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "背部",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "初級",
-    img: "./images/02.png",
-  },
-  {
-    id: "5",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "部位",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "中級",
-    img: "./images/03.jpg",
-  },
-  {
-    id: "6",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "部位",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "高級",
-    img: "./images/03.jpg",
-  },
-  {
-    id: "7",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "部位",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "低級",
-    img: "./images/03.jpg",
-  },
-  {
-    id: "8",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "部位",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "下流",
-    img: "./images/01.png",
-  },
-  {
-    id: "9",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "部位",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "高級",
-    img: "./images/02.png",
-  },
-  {
-    id: "10",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "部位",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "初級",
-    img: "./images/03.jpg",
-  },
-  {
-    id: "11",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "部位",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "中級",
-    img: "./images/01.png",
-  },
-  {
-    id: "12",
-    title: "課程名稱",
-    coach: "Taylor",
-    bodyparts: "部位",
-    count: "505",
-    created_at: "2021/10/20",
-    heart: 123,
-    level: "高級",
-    img: "./images/01.png",
-  },
-];
 let storage = sessionStorage;
 
-function CoursesCourseCard() {
+function CoursesCourseCard(item) {
   // sessionStorage
   // 給 WaitingList 空字串
   if (storage["WaitingList"] == null) {
@@ -163,9 +27,6 @@ function CoursesCourseCard() {
     }
     // 切割 input中的資料
     let waitingId = e.currentTarget.children[2].value.split("|")[0];
-    let waitingTitle = e.currentTarget.children[2].value.split("|")[1];
-    let waitingBodyParts = e.currentTarget.children[2].value.split("|")[2];
-    let waitingImg = e.currentTarget.children[2].value.split("|")[3];
     // 將資料存入 Storage 送到 課程單獨頁面
     // 如果 有 按過加入待播清單 就把相同的 刪掉 並從 WaitingList 中也一併刪除
     // 如果 sessionStorage 中沒有相同資料 就加入 WaitingList
@@ -181,71 +42,55 @@ function CoursesCourseCard() {
       storage["WaitingList"] += `${waitingId},`;
     }
   }
+  let storagelist = storage["WaitingList"].split(",");
   return (
     <>
-      {item.map((item) => {
-        let storagelist = storage["WaitingList"].split(",");
-        return (
-          <div className="" key={item.id}>
-            <div className="Courses__singlecourse__card mb-5">
-              <img src="./images/01.png" className="card-img-top" alt="課程1" />
-              <div className="card-body">
-                <div className="mb-2 d-flex">
-                  <div className="Courses__singlecourse__card__type">
-                    {item.bodyparts}
-                  </div>
-                  <div className="Courses__singlecourse__card__coach_name ms-3">
-                    {item.coach}
-                  </div>
-                </div>
-                <h3 className="mt-3">{item.title}</h3>
-                <div className="mt-4 d-flex">
-                  <div className="Courses__singlecourse__card__count me-2">
-                    觀看次數：{item.count}次
-                  </div>
-                  <div className="Courses__singlecourse__card__created-at me-2">
-                    {item.created_at}
-                  </div>
-                  <div className="Courses__singlecourse__card__heart">
-                    <span>{item.heart}</span>
-                    <i
-                      id={item.id}
-                      class="far fa-heart"
-                      onClick={clickHeart}
-                    ></i>
-                  </div>
-                </div>
+        <div className="Courses__singlecourse__card mb-5">
+          <img src="./images/01.png" className="card-img-top" alt="課程1" />
+          <div className="card-body">
+            <div className="mb-2 d-flex">
+              <div className="Courses__singlecourse__card__type">
+                {item.bodyparts}
               </div>
-              <div
-                id={item.id}
-                className="Courses__play-list"
-                onClick={AddList}
-              >
-                <div>
-                  {storagelist.includes(item.id)
-                    ? "成功加入清單"
-                    : "加入待播清單"}
-                </div>
-                <img
-                  src={
-                    storagelist.includes(item.id)
-                      ? "./images/play-list-addsuccess.png"
-                      : "./images/play-list-bg.png"
-                  }
-                  alt="play-list"
-                />
-                <input
-                  type="hidden"
-                  value={`${item.id}|${item.title}|${item.bodyparts}|${item.level}|${item.img}`}
-                />
+              <div className="Courses__singlecourse__card__coach_name ms-3">
+                {item.coach}
               </div>
-              <div className="Courses__singlecourse__card__coach Courses__singlecourse__card__coach-setting">
-                <img src="./images/03.jpg" alt="coach" />
+            </div>
+            <h3 className="mt-3">{item.title}</h3>
+            <div className="mt-4 d-flex">
+              <div className="Courses__singlecourse__card__count me-2">
+                觀看次數：{item.count}次
+              </div>
+              <div className="Courses__singlecourse__card__created-at me-2">
+                {item.created_at}
+              </div>
+              <div className="Courses__singlecourse__card__heart">
+                <span>{item.heart}</span>
+                <i id={item.id} class="far fa-heart" onClick={clickHeart}></i>
               </div>
             </div>
           </div>
-        );
-      })}
+          <div id={item.id} className="Courses__play-list" onClick={AddList}>
+            <div>
+              {storagelist.includes(item.id) ? "成功加入清單" : "加入待播清單"}
+            </div>
+            <img
+              src={
+                storagelist.includes(item.id)
+                  ? "./images/play-list-addsuccess.png"
+                  : "./images/play-list-bg.png"
+              }
+              alt="play-list"
+            />
+            <input
+              type="hidden"
+              value={`${item.id}|${item.title}|${item.bodyparts}|${item.level}|${item.img}`}
+            />
+          </div>
+          <div className="Courses__singlecourse__card__coach Courses__singlecourse__card__coach-setting">
+            <img src="./images/03.jpg" alt="coach" />
+          </div>
+        </div>
     </>
   );
 }
