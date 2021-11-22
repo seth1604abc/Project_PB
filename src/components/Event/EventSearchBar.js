cccf8f98f29f1762f8afa7196a7c1023c41597e0
Ariel Liu <regina31664@gmail.com>
2021-11-22 21:11:39 +0800

    update

--- src/components/Event/EventSearchBar.js
+++ src/components/Event/EventSearchBar.js
@@ -1,40 +1,41 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+// import "./App.css";
 
 function EventSearchBar() {
-
-    const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
   const [finishDate, setFinishDate] = useState(new Date());
 
   return (
     <div className="event_searchBar_wrapper my-5">
-    <span className="mx-5 text-white h5">活動區間</span>
-    <DatePicker
-      type="month"
-      className="event_search_month"
-      id="event_search_month"
-      name="event_search_month"
-      select= {startDate} 
-      onChange={(date) => setStartDate(date)}
-    />
-    <span className="text-white h5 mx-4">至</span>
-    <DatePicker
-      type="month"
-      className="event_search_month"
-      id="event_search_month"
-      name="event_search_month"
-      select= {finishDate} 
-      onChange={(date) => setFinishDate(date)}
-    />
-    <button type="event_search_submit" className="event_search_submit">
-      <i className="fa fa-search"></i>
-    </button>
-  </div>
-  )
-
+      <div className="event_searchBar_container">
+        <span className="mx-5 text-white h5">活動區間</span>
+        <div>
+          <DatePicker
+            className="event_search_month"
+            id="event_search_month"
+            name="event_search_month"
+            select={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+        </div>
+        <span className="text-white h5 mx-4">至</span>
+        <div>
+          <DatePicker
+            className="event_search_month"
+            id="event_search_month"
+            name="event_search_month"
+            select={finishDate}
+            onChange={(date) => setFinishDate(date)}
+          />
+        </div>
+        <button type="event_search_submit" className="event_search_submit">
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
+    </div>
+  );
 }
-
 
 export default EventSearchBar;
