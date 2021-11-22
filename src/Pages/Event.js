996f01e18c1a245e31e6e08e7d47ef9ac05b98db
Ariel Liu <regina31664@gmail.com>
2021-11-22 21:11:39 +0800

    update

--- src/Pages/Event.js
+++ src/Pages/Event.js
@@ -6,16 +6,25 @@ import EventBanner from "../components/Event/EventBanner";
 import EventCard from "../components/Event/EventCard";
 import EventSearchBar from "../components/Event/EventSearchBar";
 import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Event() {
   //這邊是Link用的，啟學所有
   //讓每次進入頁面在最上方
-  useEffect(() => {
+  useEffect(async () => {
     window.scroll({
       top: 0,
       behavior: "instant",
     });
 
+    let resEvent = await axios.get("http://localhost:3001/event", {
+      withCredentials: true,
+    });
+    setEvent(resEvent.data);
+    console.log(resEvent.data);
   }, []);
+
+  const [event, setEvent] = useState([]);
 
   const EventData = [
     {
@@ -76,13 +85,20 @@ function Event() {
       <nav>
         <EventBanner />
       </nav>
-        <div className="event_container">
-          <div>
-            <h3>訂閱會員限定 活動報名中</h3>
-          </div>
-          <EventSearchBar /> 
-         
+      <div className="event_container">
+        <div>
+          <h3>訂閱會員限定 活動報名中</h3>
         </div>
+        <EventSearchBar />
+        {EventData.map((EventData) => {
+          return <EventCard key={EventData.id} id={EventData.id} title={EventData.title} event_time_month ={EventData.event_time_month} 
+            event_time_day={EventData.event_time_day}
+            time={EventData.time}
+            quota={EventData.quota}
+            coach={EventData.coach}
+          />;
+        })}
+      </div>
       <Footer />
     </div>
   );
