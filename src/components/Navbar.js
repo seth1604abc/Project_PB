82e2a808ea1ea7daf63828547191708b838247c4
hungchihsueh <chihsuehhung@gmail.com>
2021-11-25 19:18:27 +0800

    設計購物車

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,70 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-import $ from 'jquery';
-
+import axios from "axios";
+import $ from "jquery";
 
 function Navbar() {
-  const [isLoggedin, setIsLoggedin] = useState(false);  
-  const[cartData,setCartData]=useState([])
-  useEffect( async () => {
-    let res = await axios.get("http://localhost:3001/auth/login", { withCredentials: true });    
-    if(res.data.userId) {
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [cartData, setCartData] = useState([]);
+  useEffect(async () => {
+    let res = await axios.get("http://localhost:3001/auth/login", {
+      withCredentials: true,
+    });
+    if (res.data.userId) {
       setIsLoggedin(true);
     }
-    let cartList=  await axios.get("http://localhost:3001/cart/list", { withCredentials: true });    
+    let cartList = await axios.get("http://localhost:3001/cart/list", {
+      withCredentials: true,
+    });
     setCartData(cartList.data);
     console.log(cartList.data);
-  }, [])
+  }, []);
 
   //map購物車
-  const cartList=cartData.map((item)=>{
-    return(
-        <div className="d-flex align-items-center">
-        <img src={`./product_images/${item.name}`} alt="" style={{width:"5rem"}}/>
-          <h5 className="text-nowrap">{item.title}</h5>
+  const cartList = cartData.map((item) => {
+    return (
+      <div className="d-flex align-items-center justify-content-start">
+        <div
+          className="me-1"
+          style={{ width: "5rem", backgroundColor: "white" }}
+        >
+          <img
+            src={`./product_images/${item.name}`}
+            alt=""
+            style={{ width: "5rem", fontSize: "0.8rem" }}
+          />
+        </div>
+        <div className="d-flex flex-column me-3">
+          <dic className="" style={{ width: "10rem" }}>
+            {item.title}
+          </dic>
           <div className="text-end">
-          <div>{item.amount}x</div>
-          <div>price:{`${item.price*item.amount}`}</div>
+            <div>{item.amount}x</div>
+            <div>price:{`${item.price * item.amount}`}</div>
           </div>
         </div>
-    )
-  })
-
-
-
+      </div>
+      <hr />
+    );
+  });
 
   const showDiv = () => {
-    if(document.querySelector(".navbar-login-li__div").style.display == "block") {
+    if (
+      document.querySelector(".navbar-login-li__div").style.display == "block"
+    ) {
       document.querySelector(".navbar-login-li__div").style.display = "none";
     } else {
       document.querySelector(".navbar-login-li__div").style.display = "block";
-    }   
-  }
+    }
+  };
 
   const logout = async () => {
-    let result = await axios.post("http://localhost:3001/auth/logout", { withCredentials: true });
+    let result = await axios.post("http://localhost:3001/auth/logout", {
+      withCredentials: true,
+    });
     console.log(result);
-  }
-  
+  };
+
   return (
     <div className="main-nav d-flex justify-content-between align-items-center">
       <Link to="/">
@@ -75,30 +93,38 @@ function Navbar() {
             <Link to="/cart">
               <i className="fas fa-shopping-cart"></i>
             </Link>
-            <div className="navbar__cart__content">{cartList}</div>
+            <div className="navbar__cart__content  flex-column align-items-center">
+              <div className="my-2">{cartList}</div>
+              <Link
+                to="/cart"
+                className="btn my-2 text-white"
+                style={{ backgroundColor: "#1d6cf5", width: "80%" }}
+              >
+                購買
+              </Link>
+            </div>
           </li>
-          {           
-            isLoggedin ? (
+          {isLoggedin ? (
             <li className="navbar-login-li" onClick={showDiv}>
               <span>
                 <i className="fas fa-user"></i>
               </span>
               <div className="navbar-login-li__div">
                 <div>
-                  <Link to="/member-info">
-                    會員中心
-                  </Link>
+                  <Link to="/member-info">會員中心</Link>
                 </div>
-                <div style={{marginTop: "10px"}} onClick={logout}>登出</div>
+                <div style={{ marginTop: "10px" }} onClick={logout}>
+                  登出
+                </div>
               </div>
             </li>
-            ) : (
-              <li>
-                <Link to="/login" style={{fontSize: "14px"}}>註冊/登入</Link>                
-              </li>
-            )
-          }
-          
+          ) : (
+            <li>
+              <Link to="/login" style={{ fontSize: "14px" }}>
+                註冊/登入
+              </Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
