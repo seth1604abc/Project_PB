6dccb118cd1137613245225034954882aac26fd6
hungchihsueh <chihsuehhung@gmail.com>
2021-11-29 15:42:44 +0800

    up

--- src/Pages/CartInfo.js
+++ src/Pages/CartInfo.js
@@ -4,7 +4,9 @@ import Footer from "../components/Footer";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import {ZipCodeTW} from "zipcode-tw-react";
+import { ZipCodeTW } from "zipcode-tw-react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 function CartInfo() {
   const location = useLocation();
@@ -14,20 +16,21 @@ function CartInfo() {
   const user = location.state.uData;
   const [userName, setUserName] = useState("");
   const [userPhone, setUserPhone] = useState("");
-const[checked,setChecked]=useState(false);
-  const handleName=(e)=>{
-      if(e.target.value!==userName){
-          setChecked(false);
-      }
-      setUserName(e.target.value)
-  }
-  const handlePhone=(e)=>{
-    if(e.target.value!==userPhone){
-        setChecked(false);
+  const [checked, setChecked] = useState(false);
+  const [data, setData] = useState({});
+  const handleName = (e) => {
+    if (e.target.value !== userName) {
+      setChecked(false);
     }
-      setUserPhone(e.target.value)
-  }
-  
+    setUserName(e.target.value);
+  };
+  const handlePhone = (e) => {
+    if (e.target.value !== userPhone) {
+      setChecked(false);
+    }
+    setUserPhone(e.target.value);
+  };
+
   const handleUserData = (e) => {
     if (checked) {
       setUserName("");
@@ -43,7 +46,58 @@ const[checked,setChecked]=useState(false);
   useEffect(() => {
     // console.log(location);
     console.log(list, point, total, user);
+    setData(user);
   }, []);
+
+  //處理711
+  // const handleMart = async (countyValue, districtValue) => {
+  //   let result = await axios({
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/x-www-form-urlencoded",
+  //       "Access-Control-Allow-Origin": " *",
+  //       "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+  //     },
+  //     url: "http://emap.pcsc.com.tw/EMapSDK.aspx",
+  //     data: {
+  //       commandid: "SearchStore",
+  //       city: `${countyValue}`,
+  //       town: `${districtValue}`,
+  //     },
+  //   });
+  //   console.log(result);
+  // };
+
+  //處理縣市
+  const handleCountyChange = (e) => {
+    const { countyValue } = e;
+    let newData = { ...data };
+    newData.city = countyValue;
+    setData(newData);
+  };
+
+  const handleDistrictChange = (e) => {
+    const { districtValue } = e;
+    let newData = { ...data };
+    newData.area = districtValue;
+    setData(newData);
+  };
+  //處理地址
+  const handleAddress = (e) => {
+    let newData = { ...data };
+    newData.addressDetail = e.target.value;
+    setData(newData);
+  };
+  //處理送貨方式
+  const [shipment, setShipment] = useState(1);
+  const handleShipment = (e) => {
+    setShipment(Number(e.target.value));
+  };
+  //處理付款方式
+  const [payment, setPayment] = useState(1);
+  const handlePayment = (e) => {
+    setPayment(Number(e.target.value));
+  };
   //map商品
   const buyList = list.map((item) => {
     return (
@@ -90,20 +144,35 @@ const[checked,setChecked]=useState(false);
             <div className="cart-content__title p-3">
               <h3>訂單摘要</h3>
             </div>
-            <div className="cart-content-l__info">{buyList}</div>
+            <div className="cart-content-l__info">
+              {buyList}
+              <hr />
+              <div className="text-end me-1 mb-1">使用點數:{point}</div>
+              <div className="text-end me-1">總金額:{total}</div>
+            </div>
 
             <div className="cart-content__title p-3 mt-4">
               <h3>收件人資料</h3>
             </div>
             <div className="cart-content-l__info">
               <div className="cart-content-l__info__check d-flex align-items-center">
-                <input id="memberData" type="checkbox" onChange={handleUserData} checked={checked} />
-                <label for="memberData" className="ms-2">同會員資料</label>
+                <input
+                  id="memberData"
+                  type="checkbox"
+                  onChange={handleUserData}
+                  checked={checked}
+                />
+                <label for="memberData" className="ms-2">
+                  同會員資料
+                </label>
               </div>
               <div className="cart-content-l__info__data">
                 <p>收件人名稱</p>
-                <input type="text" placeholder="請輸入收件人名稱" onChange={handleName} value={userName} 
-
+                <input
+                  type="text"
+                  placeholder="請輸入收件人名稱"
+                  onChange={handleName}
+                  value={userName}
                 />
               </div>
               <p style={{ fontSize: "14px", color: "red" }}>
@@ -111,7 +180,12 @@ const[checked,setChecked]=useState(false);
               </p>
               <div className="cart-content-l__info__data">
                 <p>收件人電話</p>
-                <input type="text" placeholder="請輸入收件人電話號碼" onChange={handlePhone} value={userPhone} />
+                <input
+                  type="text"
+                  placeholder="請輸入收件人電話號碼"
+                  onChange={handlePhone}
+                  value={userPhone}
+                />
               </div>
             </div>
           </div>
@@ -123,54 +197,51 @@ const[checked,setChecked]=useState(false);
               {/* <div className="cart-content-l__paymethod"> */}
               <div className="cart-content-l__paymethod__data">
                 <p>運送方式</p>
-                <select name="" id="">
-                  <option value="">宅配到府</option>
-                  <option value="">超商取貨</option>
+                <select name="" id="" onChange={handleShipment}>
+                  <option value="1">宅配到府</option>
+                  <option value="2">超商取貨</option>
                 </select>
               </div>
               <div className="cart-content-l__paymethod__data">
                 <p>付款方式</p>
-                <select name="" id="">
-                  <option value="">貨到付款</option>
-                  <option value="">信用卡</option>
+                <select name="" id="" onChange={handlePayment}>
+                  <option value="1">貨到付款</option>
+                  <option value="2">信用卡</option>
                 </select>
               </div>
               {/* </div> */}
 
-              <p>運送地址(宅配)</p>
-              {/* <select name="" id="">
-                <option value="">台北市</option>
-                <option value="">桃園市</option>
-              </select>
-              <select name="" id="" style={{ marginLeft: "20px" }}>
-                <option value="">東區</option>
-                <option value="">北區</option>
-              </select>
-              <div style={{ margin: "10px 0" }}>
-                <input
-                  style={{ padding: "10px", width: "95%" }}
-                  type="text"
-                  placeholder="請輸入地址"
-                />
-              </div> */}
-              <ZipCodeTW displayType="text"/>
+              <p>運送地址{shipment === 1 ? "(宅配)" : "(超商)"}</p>
+              <ZipCodeTW
+                displayType="text"
+                zipStyle={{ display: "none" }}
+                districtStyle={{ marginLeft: "20px" }}
+                districtClass="districtClass"
+                countyClass="countyClass"
+                countyValue={data.city}
+                handleChangeCounty={handleCountyChange}
+                districtValue={data.area}
+                handleChangeDistrict={handleDistrictChange}
+              />
+              <input
+                type="text"
+                className="my-3 w-75"
+                onChange={handleAddress}
+              />
 
               <p>運送地址(超商)</p>
-              <select name="" id="">
-                <option value="">台北市</option>
-                <option value="">桃園市</option>
-              </select>
-              <select name="" id="" style={{ marginLeft: "20px" }}>
-                <option value="">東區</option>
-                <option value="">北區</option>
-              </select>
-              <div style={{ margin: "10px 0" }}>
-                <input
-                  style={{ padding: "10px", width: "95%" }}
-                  type="text"
-                  placeholder="選擇超商門市"
-                />
-              </div>
+              <ZipCodeTW
+                displayType="text"
+                zipStyle={{ display: "none" }}
+                districtStyle={{ marginLeft: "20px" }}
+                districtClass="districtClass"
+                countyClass="countyClass"
+                countyValue={data.city}
+                handleChangeCounty={handleCountyChange}
+                districtValue={data.area}
+                handleChangeDistrict={handleDistrictChange}
+              />
+              <select className="form-control"></select>
 
               <p>付款方式: 貨到付款</p>
 
@@ -192,17 +263,48 @@ const[checked,setChecked]=useState(false);
                 +新增信用卡
               </p>
             </div>
-            <div className="cart-content-r__btn">
-              <button
+            <div className="cart-content-r__btn d-flex align-items-center justify-content-end">
+              <Link
+                to="cart"
                 style={{
                   marginRight: "20px",
                   backgroundColor: "#ffecd2",
                   color: "black",
+                  fontSize: "1.5rem",
                 }}
+                className="btn px-3"
               >
                 上一步
-              </button>
-              <button style={{ backgroundColor: "#2571e3", color: "white" }}>
+              </Link>
+              <button
+                className="btn px-3"
+                style={{
+                  backgroundColor: "#2571e3",
+                  color: "white",
+                  fontSize: "1.5rem",
+                }}
+                onClick={async()=>{
+                  await axios
+                  .post(`http://localhost:3001/cart/addcart/${productId}`, {
+                    number: `${number}`,
+                  })
+                  .then(function (response) {
+                    console.log(response);
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+                
+                Swal.fire({
+                  title: "成功加入購物車",
+                  text: `${productData.title} ${number}份加入購物車`,
+                  icon: "success",
+                  confirmButtonText: "繼續購物",
+                  confirmButtonColor: "#1d6cf5",
+                });
+              }
+                }
+              >
                 確認訂單
               </button>
             </div>
