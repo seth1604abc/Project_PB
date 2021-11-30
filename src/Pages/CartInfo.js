f0b59c4963438cc0fc532c24e09b1d57225af150
hungchihsueh <chihsuehhung@gmail.com>
2021-11-30 12:58:40 +0800

    Update CartInfo.js

--- src/Pages/CartInfo.js
+++ src/Pages/CartInfo.js
@@ -8,7 +8,7 @@ import { ZipCodeTW } from "zipcode-tw-react";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { FormControlUnstyled } from "@mui/core";
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
 import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
 
@@ -47,24 +47,24 @@ function CartInfo() {
       setChecked(true);
     }
   };
-useEffect(()=>{
-  setData(user);
-},[])
-  useEffect(async() => {
+  useEffect(() => {
+    setData(user);
+  }, []);
+  useEffect(async () => {
     // console.log(location);
     // console.log(list, point, total, user);
-    
-   let mart=await axios.post("http://localhost:3001/cart/mart",{
-      city:`${data.city}`,
-      area:`${data.area}`
-    }).then(function (response) {
-      console.log(response);
-      
-      // console.log($);
-      
-    });
-    
-  }, [data]);
+
+    let mart = await axios
+      .post("http://localhost:3001/cart/mart", {
+        city: `${data.city}`,
+        area: `${data.area}`,
+      })
+      .then(function (response) {
+        console.log(response);
+
+        // console.log($);
+      });
+  }, [data.city, data.area]);
   //處理信用卡
   const [cData, setCData] = useState({
     cvc: "",
@@ -72,28 +72,28 @@ useEffect(()=>{
     name: "",
     number: "",
   });
- const handleInputNumber=(e)=>{
-   console.log(e.target.value.length)
-   if(e.target.value.length>16){
-    return
-   }else{
-    setCData({ ...cData, [e.target.name]: e.target.value });
-   }
- };
+  const handleInputNumber = (e) => {
+    console.log(e.target.value.length);
+    if (e.target.value.length > 16) {
+      return;
+    } else {
+      setCData({ ...cData, [e.target.name]: e.target.value });
+    }
+  };
 
   const handleInputChange = (e) => {
     setCData({ ...cData, [e.target.name]: e.target.value });
   };
   //處理縣市
-  const handleCountyChange = async(e) => {
+  const handleCountyChange = async (e) => {
     const { countyValue } = e;
     let newData = { ...data };
     newData.city = countyValue;
-    newData.area="請選擇";
-     await setData(newData);
+    newData.area = "請選擇";
+    await setData(newData);
   };
 
-  const handleDistrictChange = async(e) => {
+  const handleDistrictChange = async (e) => {
     const { districtValue } = e;
     let newData = { ...data };
     newData.area = districtValue;
@@ -252,14 +252,17 @@ useEffect(()=>{
                 districtValue={data.area}
                 handleChangeDistrict={handleDistrictChange}
               />
-              
-              {shipment === 1 ? <input
-                type="text"
-                placeholder="請輸入詳細地址"
-                className="my-3 w-75 form-control"
-                onChange={handleAddress}
-                onClick={() => console.log(data)}
-              /> : <select
+
+              {shipment === 1 ? (
+                <input
+                  type="text"
+                  placeholder="請輸入詳細地址"
+                  className="my-3 w-75 form-control"
+                  onChange={handleAddress}
+                  onClick={() => console.log(data)}
+                />
+              ) : (
+                <select
                   className="form-select"
                   name=""
                   id=""
@@ -268,7 +271,7 @@ useEffect(()=>{
                   <option value="1">貨到付款</option>
                   <option value="2">信用卡</option>
                 </select>
-              }
+              )}
               {/* <input
                 type="text"
                 placeholder="請輸入詳細地址"
@@ -277,54 +280,52 @@ useEffect(()=>{
                 onClick={() => console.log(data)}
               /> */}
 
-              
-
-              <p>付款方式: {payment===1?"貨到付款":"信用卡"}</p>
+              <p>付款方式: {payment === 1 ? "貨到付款" : "信用卡"}</p>
               <i
                 style={{ fontSize: "18px", marginLeft: "10px" }}
                 class="fab fa-cc-visa"
               ></i>
               <div id="PaymentForm" className="creditCard-group p-3">
-               <Cards
-              cvc={cData.cvc}
-              expiry={cData.expiry}
-              focus={cData.focus}
-              name={cData.name}
-              number={cData.number}
-            />
-             <form action="" className="d-flex flex-column my-3">
-              <input
-                type="number"
-                className="form-control my-1"
-                name="cvc"
-                placeholder="CVC"
-                onChange={handleInputChange}
-                maxLength="3"
-              />
-              <input
-                type="date"
-                className="form-control my-1"
-                name="expiry"
-                placeholder="Expire Date"
-                onChange={handleInputChange}
-              />
-              <input
-                type="text"
-                className="form-control my-1"
-                name="name"
-                placeholder="Your Name"
-                onChange={handleInputChange}
-              />
-              <input
-                type="number"
-                className="form-control my-1"
-                name="number"
-                placeholder="Card Number"
-                onChange={handleInputNumber}
-                max="9999999999999999"
-                value={cData.number}
-              />
-            </form>
+                <Cards
+                  cvc={cData.cvc}
+                  expiry={cData.expiry}
+                  focus={cData.focus}
+                  name={cData.name}
+                  number={cData.number}
+                />
+                <form action="" className="d-flex flex-column my-3">
+                  <input
+                    type="number"
+                    className="form-control my-1"
+                    name="cvc"
+                    placeholder="CVC"
+                    onChange={handleInputChange}
+                    maxLength="3"
+                  />
+                  <input
+                    type="date"
+                    className="form-control my-1"
+                    name="expiry"
+                    placeholder="Expire Date"
+                    onChange={handleInputChange}
+                  />
+                  <input
+                    type="text"
+                    className="form-control my-1"
+                    name="name"
+                    placeholder="Your Name"
+                    onChange={handleInputChange}
+                  />
+                  <input
+                    type="number"
+                    className="form-control my-1"
+                    name="number"
+                    placeholder="Card Number"
+                    onChange={handleInputNumber}
+                    max="9999999999999999"
+                    value={cData.number}
+                  />
+                </form>
               </div>
               <p
                 style={{
@@ -384,13 +385,24 @@ useEffect(()=>{
                       },
                     }
                   );
-                  await axios.post(`http://localhost:3001/cart/gain-point`,{
-                    gainPoint: `${total / 100}`,
-                  })
-                  await axios.post(`http://localhost:3001/cart/add-orderdetail`,{
-                    listId:`${list.map(i=>i.product_id)}`,
-                    listAmount:`${list.map(i=>i.amount)}`
-                  })
+                  await axios.patch(
+                    `http://localhost:3001/cart/gain-point/${
+                      point - total / 100
+                    }`
+                  );
+                  for(var i=0;i<list.length;i++){
+                    if(list[i]){
+                      await axios.patch(
+                    `http://localhost:3001/cart/product-amount/${list[i].amount}/${list[i].product_id}`
+                  )
+                    }}
+                  await axios.patch(
+                    `http://localhost:3001/cart/product-amount/`
+                  )
+                  // await axios.post(`http://localhost:3001/cart/add-orderdetail`,{
+                  //   listId:`${list.map(i=>i.product_id)}`,
+                  //   listAmount:`${list.map(i=>i.amount)}`
+                  // })
                   Swal.fire({
                     title: "購買完成!",
                     text: `已成功購買${list.length}項商品`,
