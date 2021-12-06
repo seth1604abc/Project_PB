edcdad290501b6ac6b594c902e8e863539c273bc
Eddie <90377819+Eddc125@users.noreply.github.com>
2021-12-06 19:43:06 +0800

    2021/12/6 - 新增會員

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
+import { color } from '@material-ui/system'
 import React from 'react'
 
 function Footer() {
     return (
         <footer>
-            <div style={{height: "30px", width: "90%", borderBottom: "2px solid #ffecd2", margin: "0 auto"}}></div>
-            <div style={{height: "30px", width: "90%", color: "#ffecd2", margin: "0 auto"}}>&copy;P&B Inc, 2021</div>
+            <div style={{ width: "90%",padding:'1rem',textAlign:'center', margin: "0 auto", color:'var(--second-color)'}}>本網頁所載的所有資料、商標、標誌、圖像、短片、聲音檔案、連結及其他資料等（以下簡稱「資料」），只供資訊工業策進會(MFEE20)專題發表使用，如有侵權，請聯繫： y734340@gms.tku.edu.tw，我們會將資料全數下架，謝謝您。</div>
+            <div style={{ width: "90%",padding:'1rem',textAlign:'center', color: "#ffecd2", borderTop: "2px solid #ffecd2" , margin: "0 auto" }}>&copy;P&B Inc, 2021</div>
         </footer>
     )
 }
